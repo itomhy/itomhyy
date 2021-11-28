495f46020abadab0140b59601637ee006f4f9745
YushkaBots <92757522+Yushkabotss@users.noreply.github.com>
2021-11-29 00:27:43 +0400

    Update play.py

--- MusicAndVideo/play.py
+++ MusicAndVideo/play.py
@@ -17,8 +17,8 @@ from config import HNDLR, bot, call_py
 from MusicAndVideo.helpers.queues import QUEUE, add_to_queue, get_queue
 
 AMBILFOTO = [
-    "https://telegra.ph/file/a1533b98683cffaea173a.jpg",
-    "https://te.legra.ph/file/a1533b98683cffaea173a.jpg",
+    "https://telegra.ph/file/171b9100ce58bd7d12bca.jpg",
+    "https://telegra.ph/file/171b9100ce58bd7d12bca.jpg",
 ]
 
 IMAGE_THUMBNAIL = random.choice(AMBILFOTO)
